48ae34e47afd300a44ca9bf2e61f767786fb9860
ilker atabay <atabayilker@gmail.com>
2023-04-15 00:23:19 +0300

    feat: Added IsNullOrDefault extension method to make null or default checks easier.

---
+++ TvShowSite.Core/ExtensionMethods/IsNullOrDefaultExtension.cs
@@ -0,0 +1,30 @@
+﻿namespace TvShowSite.Core.ExtensionMethods
+{
+    public static class IsNullOrDefaultExtension
+    {
+        public static bool IsNullOrDefault<T>(this T argument)
+        {
+            if(argument == null)
+                return true;
+            if (object.Equals(argument, default(T)))
+                return true;
+
+            // deal with non-null nullables
+            Type methodType = typeof(T);
+
+            if (Nullable.GetUnderlyingType(methodType) != null) 
+                return false;
+
+            Type argumentType = argument.GetType();
+
+            if (argumentType.IsValueType && argumentType != methodType)
+            {
+                object? obj = Activator.CreateInstance(argument.GetType());
+
+                return obj == null ? true : obj.Equals(argument);
+            }
+
+            return false;
+        }
+    }
+}
