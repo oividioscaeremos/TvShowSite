6f36e6606a23bfec7f8cd5900b0756c55c54f6e4
ilker atabay <atabayilker@gmail.com>
2023-04-15 00:24:01 +0300

    add: Added SkipInsert attribute to use on generic Insert/InsertAsync methods.

---
+++ TvShowSite.Domain/Attributes/SkipInsert.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TvShowSite.Domain.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class SkipInsert : Attribute
+    {
+        public SkipInsert()
+        {
+            
+        }
+    }
+}
